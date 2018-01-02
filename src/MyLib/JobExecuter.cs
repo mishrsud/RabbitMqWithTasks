0de93af2904aee2fc1718c80f343083f2240c45a
Sudhanshu Mishra <mailsudhanshumishra@gmail.com>
2018-01-02 17:55:16 +1100

    Initial working version

---
+++ src/MyLib/JobExecuter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MyLib
+{
+    public class JobExecuter
+    {
+        public void DoWork(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Working, ThreadPool: {Thread.CurrentThread.IsThreadPoolThread}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "admin"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                // EventingBasicConsumer fires the handler subscribed to consumer.Received event
+                // As long as the thread containing this code is running
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+                };
+                channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
+
+                cancellationToken.WaitHandle.WaitOne();
+                Console.WriteLine($"Exiting consumer, ThreadPool: {Thread.CurrentThread.IsThreadPoolThread}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            }
+        }
+    }
+}
---
+++ src/Startup/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MyLib;
+using RabbitMQ.Client;
+
+namespace Startup
+{
+    class Program
+    {
+        private static CancellationTokenSource _cancellationTokenSource;
+
+        static void Main(string[] args)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = _cancellationTokenSource.Token;
+
+            Task.Run(() =>
+            {
+                Send();
+            }, cancellationToken);
+
+            /* RUN AS A DEDICATED THREAD */
+            //Task.Factory.StartNew(() =>
+            //{
+            //    var executor = new JobExecuter();
+            //    executor.DoWork(cancellationToken);
+            //}, TaskCreationOptions.LongRunning);
+
+            /* RUN ON A THREAD POOL THREAD */
+            Task.Run(() =>
+            {
+                var executor = new JobExecuter();
+                executor.DoWork(cancellationToken);
+            }, cancellationToken);
+
+
+            Console.WriteLine("In Main, Done, Press enter to stop sending messages");
+            Console.ReadLine();
+            _cancellationTokenSource.Cancel();
+            Console.WriteLine("After cancellation, press any key to terminate");
+
+            Console.ReadLine();
+        }
+
+        private static void Send()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "admin"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                while (!_cancellationTokenSource.IsCancellationRequested)
+                {
+                    string message = $"Hello RabbitMQ! {DateTime.UtcNow}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine("Message sent");
+                    Console.WriteLine("--------------");
+                    Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+                }
+            }
+        }
+    }
+}
